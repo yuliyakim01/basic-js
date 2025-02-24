36263e73665d0a15fb2aff936d4e220e1283bdf9
yuliyakim01 <yulia.kim@du.edu>
2025-02-24 08:04:35 +0500

    feat: count-cats and carbon-dating implement

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string") {
+    return false;
+  }
+  let activity = parseFloat(sampleActivity);
+
+  if (isNaN(activity) || activity <= 0 || activity > MODERN_ACTIVITY) {
+    return false;
+  }
+
+  let decayConstant = 0.693 / HALF_LIFE_PERIOD;
+
+  let age = Math.log((MODERN_ACTIVITY/activity))/decayConstant;
+  return Math.ceil(age);
 }
 
 module.exports = {
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let catCounter = 0;
+  let target = '^^';
+  if (matrix === null) {
+    return 0;
+  }
+
+  matrix.forEach(row => {
+    row.forEach(cat => {
+      if (cat === target) {
+        catCounter++;
+      }
+    });
+  });
+  return catCounter;
 }
 
 module.exports = {
